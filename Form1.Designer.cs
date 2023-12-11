f2f676bbc2c720f9d79ef7fd0d9959518f11569c
hasanbadr <hasan.badr@edu.keuda.fi>
2023-12-11 22:35:55 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Hasan.Badr.harjoitus17
+{
+    partial class NotepadForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TekstiTB = new RichTextBox();
+            menuStrip1 = new MenuStrip();
+            tiedostoToolStripMenuItem = new ToolStripMenuItem();
+            uusiToolStripMenuItem = new ToolStripMenuItem();
+            avaaToolStripMenuItem = new ToolStripMenuItem();
+            tallennaToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripMenuItem();
+            lopetaToolStripMenuItem = new ToolStripMenuItem();
+            muotoileToolStripMenuItem = new ToolStripMenuItem();
+            kirjasinToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // TekstiTB
+            // 
+            TekstiTB.Dock = DockStyle.Fill;
+            TekstiTB.Location = new Point(0, 24);
+            TekstiTB.Name = "TekstiTB";
+            TekstiTB.Size = new Size(800, 426);
+            TekstiTB.TabIndex = 0;
+            TekstiTB.Text = "";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { tiedostoToolStripMenuItem, muotoileToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // tiedostoToolStripMenuItem
+            // 
+            tiedostoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { uusiToolStripMenuItem, avaaToolStripMenuItem, tallennaToolStripMenuItem, toolStripMenuItem2, lopetaToolStripMenuItem });
+            tiedostoToolStripMenuItem.Name = "tiedostoToolStripMenuItem";
+            tiedostoToolStripMenuItem.Size = new Size(65, 20);
+            tiedostoToolStripMenuItem.Text = "Tiedosto";
+            // 
+            // uusiToolStripMenuItem
+            // 
+            uusiToolStripMenuItem.Name = "uusiToolStripMenuItem";
+            uusiToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.N;
+            uusiToolStripMenuItem.Size = new Size(179, 22);
+            uusiToolStripMenuItem.Text = "Uusi";
+            uusiToolStripMenuItem.Click += uusiToolStripMenuItem_Click;
+            // 
+            // avaaToolStripMenuItem
+            // 
+            avaaToolStripMenuItem.Name = "avaaToolStripMenuItem";
+            avaaToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.O;
+            avaaToolStripMenuItem.Size = new Size(179, 22);
+            avaaToolStripMenuItem.Text = "Avaa";
+            avaaToolStripMenuItem.Click += avaaToolStripMenuItem_Click;
+            // 
+            // tallennaToolStripMenuItem
+            // 
+            tallennaToolStripMenuItem.Name = "tallennaToolStripMenuItem";
+            tallennaToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.S;
+            tallennaToolStripMenuItem.Size = new Size(179, 22);
+            tallennaToolStripMenuItem.Text = "Tallenna";
+            tallennaToolStripMenuItem.Click += tallennaToolStripMenuItem_Click;
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(179, 22);
+            toolStripMenuItem2.Text = "_____________________";
+            // 
+            // lopetaToolStripMenuItem
+            // 
+            lopetaToolStripMenuItem.Name = "lopetaToolStripMenuItem";
+            lopetaToolStripMenuItem.ShortcutKeys = Keys.Alt | Keys.F4;
+            lopetaToolStripMenuItem.Size = new Size(179, 22);
+            lopetaToolStripMenuItem.Text = "Lopeta";
+            lopetaToolStripMenuItem.Click += lopetaToolStripMenuItem_Click;
+            // 
+            // muotoileToolStripMenuItem
+            // 
+            muotoileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { kirjasinToolStripMenuItem });
+            muotoileToolStripMenuItem.Name = "muotoileToolStripMenuItem";
+            muotoileToolStripMenuItem.Size = new Size(67, 20);
+            muotoileToolStripMenuItem.Text = "Muotoile";
+            // 
+            // kirjasinToolStripMenuItem
+            // 
+            kirjasinToolStripMenuItem.Name = "kirjasinToolStripMenuItem";
+            kirjasinToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.Shift | Keys.F;
+            kirjasinToolStripMenuItem.Size = new Size(184, 22);
+            kirjasinToolStripMenuItem.Text = "Kirjasin";
+            kirjasinToolStripMenuItem.Click += kirjasinToolStripMenuItem_Click;
+            // 
+            // NotepadForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(TekstiTB);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "NotepadForm";
+            Text = "Pinen Muistio";
+            Load += NotepadForm_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox TekstiTB;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem tiedostoToolStripMenuItem;
+        private ToolStripMenuItem uusiToolStripMenuItem;
+        private ToolStripMenuItem avaaToolStripMenuItem;
+        private ToolStripMenuItem tallennaToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItem2;
+        private ToolStripMenuItem lopetaToolStripMenuItem;
+        private ToolStripMenuItem muotoileToolStripMenuItem;
+        private ToolStripMenuItem kirjasinToolStripMenuItem;
+    }
+}
